e1e6534ac0448e21501f42adb1abc8bf0d187cc4
Robi <robi.maliqi@hotmail.co.uk>
2022-09-08 11:19:40 +0100

    more refactoring

--- js/src/name-normalizer.js
+++ js/src/name-normalizer.js
@@ -4,18 +4,22 @@ export const normalize = (name) => {
     return name;
   }
 
-  const firstName = names[0];
-  const lastName = names[names.length - 1];
+  const firstName = first(names);
+  const lastName = last(names);
   const middleNames = names.slice(1, -1);
   const middleInitials = middleNames.map(initialize);
-  const namesOtherThanLast = [firstName, ...middleInitials];
 
+  const namesOtherThanLast = [firstName, ...middleInitials];
   return `${lastName}, ${namesOtherThanLast.join(" ")}`;
 };
-const initialize = (n) => {
-  if (n.length > 1) {
-    return `${n.substring(0, 1)}.`;
+
+const first = (array) => array[0];
+const last = (array) => array[array.length - 1];
+
+const initialize = (name) => {
+  if (name.length > 1) {
+    return `${name.substring(0, 1)}.`;
   } else {
-    return n;
+    return name;
   }
 };
